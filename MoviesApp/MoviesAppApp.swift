2f540027c5080d854253378b0aa3e2b5d6e38e0d
angelatasik <angelatasik08@gmail.com>
2026-04-15 20:09:54 +0200

    Initial Commit

---
+++ MoviesApp/MoviesAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  MoviesAppApp.swift
+//  MoviesApp
+//
+//  Created by Angela Tasikj on 15.4.26.
+//
+
+import SwiftUI
+
+@main
+struct MoviesAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ MoviesAppTests/MoviesAppTests.swift
@@ -0,0 +1,19 @@
+//
+//  MoviesAppTests.swift
+//  MoviesAppTests
+//
+//  Created by Angela Tasikj on 15.4.26.
+//
+
+import Testing
+@testable import MoviesApp
+
+struct MoviesAppTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+        // Swift Testing Documentation
+        // https://developer.apple.com/documentation/testing
+    }
+
+}
